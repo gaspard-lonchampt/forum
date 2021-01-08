6fbaa3e3ed3ea9b7761852fb80ce985e06862435
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2021-01-08 11:49:22 +0100

    V0.2 avec pages conv

---
+++ pages/conversation.php
@@ -0,0 +1,9 @@
+<?php 
+ require ('../include/pages/head.php'); 
+ require ('../include/pages/naviguation.php'); 
+require ('../include/pages/conversation.php'); 
+?>
+
+<?php
+require ('../include/pages/footer.php'); 
+?>
