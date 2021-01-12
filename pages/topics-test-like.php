9539f9f4943379575c09ac31e891d42962f65208
alexandre leroux <alexandre.leroux.photographies@gmail.com>
2021-01-12 18:12:31 +0100

    base avec affichage et première condition

--- pages/topics-test-like.php
+++ pages/topics-test-like.php
@@ -15,29 +15,23 @@ catch (Exception $e)
 }
 
 
-$id_user = 1;
-$id_message = 2;
+$id_user = 2;
+$id_message = 4;
 
 // $req = $bdd->query('SELECT * FROM aime');
 // echo '<pre>';
 // var_dump($req);
 // echo '</pre>';
 
-$req = $bdd->prepare(' SELECT * FROM aime WHERE id = :id ');//on va chercher dans la bdd si le login existe déjà
-$req->execute(array( 'id' => $id_user   ));
-$donnees = $req->fetch();
+$req = $bdd->prepare(' SELECT * FROM aime WHERE id_user = :id and id_message = :id_mess ');
+$req->execute(array( 'id' => $id_user , 'id_mess' => $id_message ));
+$donnees = $req->fetch(PDO::FETCH_ASSOC);
 
 
-
-
-echo '<pre>';
-var_dump($donnees);
-echo '</pre>';
-
+if ($donnees == null )
+{
 
 ?>
-
-
 </br></br></br></br></br></br></br></br></br>
 <div class="container d-block ">
 <div class="row">
@@ -52,6 +46,12 @@ echo '</pre>';
                     <div class="text-muted small ml-3">
                         <div>Nombre de conversations : <strong>test</strong></div>
                         <div>Nombre de messages : <strong>test</strong></div>
+                        <div>
+                            <a href=""  title="j'aime"><img class="img_like" src="../img/like.png" alt=""></a>
+                            <p></p>
+                            <a href=""  title="je n'aime pas"><img class="img_like" src="../img/dislike.png" alt=""></a>
+                            <p></p>
+                        </div>
                         </br>
                     </div>
                 </div>
@@ -60,6 +60,43 @@ echo '</pre>';
     </div>
 </div>
 </div>
+<?php
+}
+
+echo '<pre>';
+print_r($donnees);
+echo '</pre>';
+
+
+?>
+
+
+<!-- </br></br></br></br></br></br></br></br></br>
+<div class="container d-block ">
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mb-4">
+
+            <div class="card-header">
+                <div class="media flex-wrap w-100 align-items-center"> <img src="../img/fuck-cat.jpg" class="d-block ui-w-40 rounded-circle" alt="">
+                    <div class="media-body ml-3"> <a href="conversation.php?id=</a>
+                        <div class="text-muted small">test</div>
+                    </div>
+                    <div class="text-muted small ml-3">
+                        <div>Nombre de conversations : <strong>test</strong></div>
+                        <div>Nombre de messages : <strong>test</strong></div>
+                        <div>
+                            <a href=""><img class="img_like" src="../img/like.png" alt=""></a>
+                            <a href=""><img class="img_like" src="../img/dislike.png" alt=""></a>
+                        </div>
+                        </br>
+                    </div>
+                </div>
+            </div>     
+        </div>
+    </div>
+</div>
+</div> -->
 
 <?php 
 require ('../include/pages/footer.php'); 
