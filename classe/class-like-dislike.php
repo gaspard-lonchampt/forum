7ca629566ac4ebee0b24c62f2b20ade5478ee19b
prénom nom <alexandre.leroux@laplateforme.io>
2021-01-13 16:51:01 +0100

    classe avance

--- classe/class-like-dislike.php
+++ classe/class-like-dislike.php
@@ -42,6 +42,29 @@ public function compte_nombre_dislike($variable)
         return $req2->fetch();
     }
 
+public function affiche_bouton_sans_like_ni_dislike($variable_1, $variable_2, $variable_3,$variable_4)
+    {
+        $bdd = $this->connection_bdd();
+        $req = $bdd->prepare(' SELECT * FROM aime WHERE id_user = :id and id_message = :id_mess ');
+        $req->execute(array( 'id' => $variable_1 , 'id_mess' => $variable_2 ));
+        $resultat_null = $req->fetch(PDO::FETCH_ASSOC);
 
+        if ($resultat_null == null )
+        {
+
+        ?>
+        </br></br></br></br></br></br></br></br></br>
+     
+
+                                <div class="d-flex h-25">
+                                    <a href="ajout_like.php"  title="j'aime"><img class="img_like" src="../img/like.png" alt=""></a>
+                                    <p><?php echo ' '. $variable_3[0]; ?> </p>
+                                    <a href="ajout_dislike.php"  title="je n'aime pas"><img class="img_like" src="../img/dislike.png" alt=""></a>
+                                    <p><?php echo ' '. $variable_4[0]; ?></p>
+                                </div>
+           
+        <?php
+        }
+    }
 
 }
--- pages/topics-test-like.php
+++ pages/topics-test-like.php
@@ -29,6 +29,7 @@ $nombre_like = $like->compte_nombre_like($id_message);
 
 $nombre_dislike = $like->compte_nombre_dislike($id_message);
 
+$like->affiche_bouton_sans_like_ni_dislike($id_user, $id_message,$nombre_like,$nombre_dislike );
 // $req = $bdd->query('SELECT * FROM aime');
 // echo '<pre>';
 // var_dump($req);
@@ -48,53 +49,8 @@ $nombre_dislike = $like->compte_nombre_dislike($id_message);
 
 
 
-
-
-
-
-
-
 // affichage si pas de LIKE ni de DISLIKE
-$req = $bdd->prepare(' SELECT * FROM aime WHERE id_user = :id and id_message = :id_mess ');
-$req->execute(array( 'id' => $id_user , 'id_mess' => $id_message ));
-$resultat_null = $req->fetch(PDO::FETCH_ASSOC);
 
-if ($resultat_null == null )
-{
-
-?>
-</br></br></br></br></br></br></br></br></br>
-<div class="container d-block ">
-<div class="row">
-    <div class="col-md-12">
-        <div class="card mb-4">
-
-            <div class="card-header">
-                <div class="media flex-wrap w-100 align-items-center"> <img src="../img/fuck-cat.jpg" class="d-block ui-w-40 rounded-circle" alt="">
-                    <div class="media-body ml-3"> <a href="conversation.php?id=</a>
-                        <div class="text-muted small">test</div>
-                    </div>
-                    <div class="text-muted small ml-3">
-                        <div>Nombre de conversations : <strong>test</strong></div>
-                        <div>Nombre de messages : <strong>test</strong></div>
-
-                        <div class="d-flex h-25">
-                            <a href="ajout_like.php"  title="j'aime"><img class="img_like" src="../img/like.png" alt=""></a>
-                            <p><?php echo ' '. $nombre_like[0]; ?> </p>
-                            <a href="ajout_dislike.php"  title="je n'aime pas"><img class="img_like" src="../img/dislike.png" alt=""></a>
-                            <p><?php echo ' '. $nombre_dislike[0]; ?></p>
-                        </div>
-                        
-                        </br>
-                    </div>
-                </div>
-            </div>     
-        </div>
-    </div>
-</div>
-</div>
-<?php
-}
 
 
 
