ed310ca390e7c50ca368967a66ce0f0188714020
prénom nom <alexandre.leroux@laplateforme.io>
2021-01-15 15:16:02 +0100

    lien enlevés sur page index

--- include/pages/index.php
+++ include/pages/index.php
@@ -18,23 +18,23 @@
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="post-preview">
-          <a href="post.html">
+          <div>
             <h2 class="post-title">
             En informatique, un développeur1 ou analyste programmeur est un informaticien qui réalise des logiciels et les met en œuvre à l'aide de langages de programmation.
-            </h2>
+            </h2></br>
             <h3 class="post-subtitle">
               Le développeur informatique (Web), développeur web 5,6 responsable des codes sources constituant les différents langages de programmation Web
             </h3>
-          </a>
+            </div></br>
           <p class="post-meta">Dans la langue française, il n'y a pas de mot spécifique aujourd'hui pour parler d'un auteur de logiciel, le terme de développeur sectoriel s'est progressivement imposé.</p>
         </div>
         <hr>
         <div class="post-preview">
-          <a href="post.html">
+        <div>
             <h2 class="post-title">
             Pour répondre aux besoins d'un client, il établit dans un premier temps un « cahier des charges » qui détermine les besoins du client en matière d'informatisation et contrôle
-            </h2>
-          </a>
+            </h2></br>
+            </div>
           <p class="post-meta">Il écrit ensuite des lignes de code nécessaires au correct fonctionnement (programmation), participe aux phases d'essais, réalise la documentation technique, s'occupe du suivi et de la maintenance de son produit. Il peut également former les utilisateurs.</p>
         </div>
  
